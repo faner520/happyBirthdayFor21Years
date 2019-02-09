21dd86da78bf780c7143215bba513320d064092b
Akanemiku <806394913@qq.com>
2019-02-09 18:44:22 +0800

    add

--- js/login.js
+++ js/login.js
@@ -1,34 +1,41 @@
-$('#login-button').click(function (event) {
-	var userName=document.getElementById("userName").value;
-    var pwd=document.getElementById("pwd").value;
-		//修改密码请改此处
-    if(userName=="123" &&  pwd=="123"){
-			event.preventDefault();
-			$('form').fadeOut(500);
-			$('.wrapper').addClass('form-success');
-			requestFullScreen();
-			setTimeout(function(){
-				location.href="BirthdayCake.html";
-			},2000);
-		}
-	else{
-		alert("Wrong Password");
-	}
-});
 
-function requestFullScreen(element) {
-	var element=document.documentElement;
-	var requestMethod = element.requestFullScreen || //W3C
-	element.webkitRequestFullScreen || //Chrome等
-	element.mozRequestFullScreen || //FireFox
-	element.msRequestFullScreen; //IE11
-	if (requestMethod) {
-		requestMethod.call(element);
-	}
-	else if (typeof window.ActiveXObject !== "undefined") {//for Internet Explorer
-		var wscript = new ActiveXObject("WScript.Shell");
-		if (wscript !== null) {
-		 wscript.SendKeys("{F11}");
-		}
-	}
-}
+function cambiar_login() {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
+document.querySelector('.cont_form_login').style.display = "block";
+document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+
+setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);  
+  
+setTimeout(function(){    
+document.querySelector('.cont_form_sign_up').style.display = "none";
+},200);  
+  }
+
+function cambiar_sign_up(at) {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+  document.querySelector('.cont_form_sign_up').style.display = "block";
+document.querySelector('.cont_form_login').style.opacity = "0";
+  
+setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
+},100);  
+
+setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
+},400);  
+
+
+}    
+
+
+
+function ocultar_login_sign_up() {
+
+document.querySelector('.cont_forms').className = "cont_forms";  
+document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+document.querySelector('.cont_form_login').style.opacity = "0"; 
+
+setTimeout(function(){
+document.querySelector('.cont_form_sign_up').style.display = "none";
+document.querySelector('.cont_form_login').style.display = "none";
+},500);  
+  
+  }
